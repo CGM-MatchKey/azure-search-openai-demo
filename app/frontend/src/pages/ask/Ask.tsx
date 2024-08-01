ff94e76603833ff894663dbb72a13ceb873a1270
CGM-MatchKey <ChrisMorgan@MatchKey.ai>
2024-08-01 13:02:53 +0000

    Data commit

--- app/frontend/src/pages/ask/Ask.tsx
+++ app/frontend/src/pages/ask/Ask.tsx
@@ -241,7 +241,7 @@ export function Component(): JSX.Element {
                 <h1 className={styles.askTitle}>Ask your data</h1>
                 <div className={styles.askQuestionInput}>
                     <QuestionInput
-                        placeholder="Example: Does my plan cover annual eye exams?"
+                        placeholder="Example: How many flats will be built?"
                         disabled={isLoading}
                         initQuestion={question}
                         onSend={question => makeApiRequest(question)}
